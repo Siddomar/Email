c89c2df57efb3433feab73e87f04d36a3fd0f2a6
Sidharth Omar <SO001122@IND.FONANTRIX.COM>
2017-11-01 16:48:00 +0530

    Initial Commmit

---
+++ app/src/main/java/com/fon/email/Config.java
@@ -0,0 +1,10 @@
+package com.fon.email;
+
+/**
+ * Created by SO001122 on 01-11-2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="";
+    public static final String PASSWORD ="";
+}
